d4a572b80ed65573924c2efe6ecdfe2026f3b390
wisin007castro <castrow666@gmail.com>
2018-02-27 11:25:43 -0400

    header

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>IA</b></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Infoactiva</b></span>
+      <span class="logo-lg"><b>Infodat</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
